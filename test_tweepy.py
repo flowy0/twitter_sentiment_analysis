420be1b3a16231c09504197b97b1bf24bf2e4279
Gayle <9532712+flowy0@users.noreply.github.com>
2021-04-12 15:40:48 +0800

    added csv output

--- test_tweepy.py
+++ test_tweepy.py
@@ -1,13 +1,15 @@
 import tweepy
 from dotenv import dotenv_values
 import logging
+import csv
 import utils
+
 
 LOGGER = logging.getLogger(__name__)
 logging.basicConfig(level='INFO')
 
 
-def get_public_tweets(config):
+def get_public_tweets(config, ):
     consumer_key = config['API_KEY']
     consumer_secret = config['API_KEY_SECRET']
     access_token_key = config['ACCESS_TOKEN']
@@ -17,6 +19,8 @@ def get_public_tweets(config):
     auth.set_access_token(access_token_key, access_token_secret)
     api = tweepy.API(auth)
 
+    filename='data/test_tweets.csv'
+
     public_tweets = api.home_timeline()
     LOGGER.info(f"data type: {type(public_tweets)}")
     LOGGER.info(f"number of tweets: {len(public_tweets)}")
@@ -25,7 +29,18 @@ def get_public_tweets(config):
         # LOGGER.info(tweet.text)
         tweets_dict['tweet'].append(tweet.text)
 
+    outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in public_tweets]
+    # write the csv
+    with open(filename, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(["id", "created_at", "text"])
+        writer.writerows(outtweets)
+    LOGGER.info(f"data saved to {filename}")
+
     return tweets_dict
+
+
+
 
 
 if __name__ == '__main__':
